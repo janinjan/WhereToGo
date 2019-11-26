e5aeee5886eef8f8a4eebd3a1379cd09d62c2ce0
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2019-11-27 00:39:00 +0100

    Set MVC architecture and add AppIcon

---
+++ WhereToGo/Controller/BaseScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseScreenViewController.swift
+//  WhereToGo
+//
+//  Created by Janin Culhaoglu on 27/11/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import UIKit
+
+class BaseScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
