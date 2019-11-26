7f0c788f4c21b92b238100b032fb3daea97d2e7d
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2019-11-26 23:17:14 +0100

    Project initialization

---
+++ WhereToGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereToGo
+//
+//  Created by Janin Culhaoglu on 26/11/2019.
+//  Copyright © 2019 Janin Culhaoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
